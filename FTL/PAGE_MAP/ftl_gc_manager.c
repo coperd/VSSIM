71b5126dccee02bd08cb690534a1455f8c4c6ab8
Coperd <lhcwhu@gmail.com>
2016-04-23 16:49:27 -0500

    Perfect Mode: noGC, we simply set GC_TIME to be zero, thus VSSIM will still   handle GC_CHECK, but there is no delay added to each GC operation

--- FTL/PAGE_MAP/ftl_gc_manager.c
+++ FTL/PAGE_MAP/ftl_gc_manager.c
@@ -34,7 +34,7 @@ void GC_CHECK(IDEState *s, unsigned int phy_flash_num,
 #endif
                 break;
             } else {
-                //s->bs->gc_whole_endtime += ssd->gc_time; // 1ms
+                s->bs->gc_whole_endtime += ssd->gc_time; // 1ms
                 ssd->gc_cnt++;
 #ifdef DEBUG_GC
                 mylog("%s GC[%d], blocking to %" PRId64 "\n", get_ssd_name(s), 
@@ -159,9 +159,11 @@ int GARBAGE_COLLECTION(IDEState *s)
     WRITE_LOG(szTemp);
 #endif
 
+#if 0
     s->bs->gc_whole_endtime += copy_page_nb * (ssdconf->cell_read_delay +
             ssdconf->reg_write_delay + ssdconf->cell_program_delay +
             ssdconf->reg_read_delay) + ssdconf->block_erase_delay;
+#endif
 
 
 #ifdef FTL_DEBUG
